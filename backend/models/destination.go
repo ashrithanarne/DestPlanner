b3295bffefc8f82afcff29c97fe9ffa155f8a07a
Ashritha Narne <ashrithanarne@gmail.com>
2026-03-21 18:03:54 -0400

    Completed destination and bookmark changes

--- backend/models/destination.go
+++ backend/models/destination.go
@@ -7,3 +7,12 @@ type Destination struct {
 	Budget      float64 `json:"budget"`
 	Description string  `json:"description"`
 }
+
+type DestinationResponse struct {
+	ID           int     `json:"id"`
+	Name         string  `json:"name"`
+	Country      string  `json:"country"`
+	Budget       float64 `json:"budget"`
+	Description  string  `json:"description"`
+	IsBookmarked bool    `json:"is_bookmarked"`
+}
